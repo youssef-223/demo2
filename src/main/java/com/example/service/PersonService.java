afdf68336b13e7ebf7f16f07f21558486f1c9ee2
youssef-223 <109833289+youssef-223@users.noreply.github.com>
2024-07-15 16:53:13 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,13 +40,6 @@ public class PersonService {
 		p.setLastName("shalaby");
 		persons.add(p);
 
-		p = new Person();
-		p.setId("5");
-		p.setAge(21);
-		p.setFirstName("joe");
-		p.setLastName("test");
-		persons.add(p);
-		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
